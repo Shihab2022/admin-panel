3013caffb192497aa4c2d8ddb8014fb90d5afc65
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2023-05-26 08:21:25 +0600

    add search option

--- src/components/table/serverTable.js
+++ src/components/table/serverTable.js
@@ -6,17 +6,19 @@ const ServerSidePaginationTable = (props) => {
   const { TABLE_DATA, columns } = props;
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(0);
-
+  const [searchData, setSearchData] = useState('');
   const pageSizeHandle = useMemo(() => {
     let pageInfo = {};
     if (TABLE_DATA.length < 10) {
       pageInfo = { pageSize: TABLE_DATA.length, pageOption: [TABLE_DATA.length] };
     } else if (TABLE_DATA.length < 20) {
       pageInfo = { pageSize: 10, pageOption: [10, TABLE_DATA.length] };
+    } else if (TABLE_DATA.length < 30) {
+      pageInfo = { pageSize: 10, pageOption: [10, 20, TABLE_DATA.length] };
     } else {
       pageInfo = { pageSize: 10, pageOption: [10, 20, 30, 50] };
     }
-    setPageSize(pageInfo?.pageSize)
+    setPageSize(pageInfo?.pageSize);
     return pageInfo;
   }, [TABLE_DATA]);
   return (
@@ -34,7 +36,8 @@ const ServerSidePaginationTable = (props) => {
         }}
         data={TABLE_DATA}
         onChangePage={(newPage) => setPage(newPage)}
-        onChangeRowsPerPage={(pageSize)=>setPageSize(pageSize)}
+        onChangeRowsPerPage={(pageSize) => setPageSize(pageSize)}
+        onSearchChange={(e) => setSearchData(e)}
       />
     </>
   );
